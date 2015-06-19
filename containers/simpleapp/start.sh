f679a6f8ec93f11427498422201c61d715af3296
plane123 <plane123@gmail.com>
2015-06-18 20:12:49 -0400

    adding more directories

---
+++ containers/simpleapp/start.sh
@@ -0,0 +1,11 @@
+cd /var/www
+
+# remove repo if it already exists
+rm -rf simple-nodejs-server; true
+
+# install latest nodejs server
+git clone http://github.com/timfpark/simple-nodejs-server simple-nodejs-server
+cd simple-nodejs-server
+npm install
+
+node server.js
